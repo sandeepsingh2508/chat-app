6ed50ebd9f1ee886da582b6b10fa8749d822026a
unknown <prasan.250899@gmail.com>
2023-01-29 22:19:19 +0530

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtIiqIT1pqdnQah14vsh8VgTV-gBXeUYo",
+  authDomain: "chatapp-be241.firebaseapp.com",
+  projectId: "chatapp-be241",
+  storageBucket: "chatapp-be241.appspot.com",
+  messagingSenderId: "39780475294",
+  appId: "1:39780475294:web:54310d50f262a64154368f"
+};
+
+export const app = initializeApp(firebaseConfig);
